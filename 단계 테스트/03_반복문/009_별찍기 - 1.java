1f83e55ab450e88a9ddeff8f18535cbd6120a4e6
InCheol Kim <84650497+rdc6087940@users.noreply.github.com>
2024-03-13 18:21:05 +0900

    Create 009_별찍기 - 1.java

---
+++ 단계 테스트/03_반복문/009_별찍기 - 1.java
@@ -0,0 +1,19 @@
+import java.util.*;
+
+public class Main {
+  public static void main(String args[]) {
+
+    Scanner sc = new Scanner(System.in);
+
+    int t = sc.nextInt();
+
+    for (int i = 0; i < t; i++) {
+      for (int k = 1; k <= i + 1; k++) {
+        System.out.print("*");
+      }
+      System.out.println("");
+    }
+
+  }
+
+}
