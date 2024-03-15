f59accb59029ed96a324dfd47f2a6e5a4127c765
InCheol Kim <84650497+rdc6087940@users.noreply.github.com>
2024-03-15 13:11:29 +0900

    Create 004_아스키 코드.java

---
+++ 단계 테스트/05_문자열/004_아스키 코드.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+public class Main {
+  public static void main(String args[]) {
+
+    Scanner sc = new Scanner(System.in);
+
+    char w = sc.next().charAt(0);
+    int asc = w;
+  
+    System.out.println(asc);
+
+  }
+}
