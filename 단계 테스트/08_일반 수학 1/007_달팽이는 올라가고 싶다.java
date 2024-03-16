2c1d89a0b29c516840b2891e86d018f14e5b75fe
InCheol Kim <84650497+rdc6087940@users.noreply.github.com>
2024-03-16 17:17:35 +0900

    Create 007_달팽이는 올라가고 싶다.java

---
+++ 단계 테스트/08_일반 수학 1/007_달팽이는 올라가고 싶다.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+ 
+public class Main {
+ 
+	public static void main(String[] args) {
+ 
+		Scanner in = new Scanner(System.in);
+ 
+		int up = in.nextInt();		
+		int down = in.nextInt();	
+		int length = in.nextInt(); 	
+ 
+		int day = (length - down) / (up - down);
+        
+		if ((length - down) % (up - down) != 0) {
+			day++;
+		}
+		System.out.println(day);
+	}
+}
