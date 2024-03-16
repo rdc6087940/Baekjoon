86cb73d12eff403f934e4468b46bcf775c6fd464
InCheol Kim <84650497+rdc6087940@users.noreply.github.com>
2024-03-16 17:11:17 +0900

    Create 001_행렬 덧셈.java

---
+++ 단계 테스트/07_2차원 배열/001_행렬 덧셈.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+
+public class Main {
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+
+    int a = sc.nextInt();
+    int b = sc.nextInt();
+
+    int[][] arr = new int[a][b];
+    int[][] brr = new int[a][b];
+
+    for (int i = 0; i < a; i++) {
+      for (int j = 0; j < b; j++) {
+        arr[i][j] = sc.nextInt();
+      }
+    }
+    for (int i = 0; i < a; i++) {
+      for (int j = 0; j < b; j++) {
+        brr[i][j] = sc.nextInt();
+      }
+    }
+    for (int i = 0; i < a; i++) {
+      for (int j = 0; j < b; j++) {
+        System.out.print(arr[i][j] + brr[i][j] + " ");
+      }
+      System.out.println("");
+    }
+
+    sc.close();
+  }
+}
