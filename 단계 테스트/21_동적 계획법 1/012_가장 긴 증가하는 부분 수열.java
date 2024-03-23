d08150d03f4988b369cb2e8f53bb217c43dddf24
InCheol Kim <84650497+rdc6087940@users.noreply.github.com>
2024-03-23 16:12:30 +0900

    Create 012_가장 긴 증가하는 부분 수열.java

---
+++ 단계 테스트/21_동적 계획법 1/012_가장 긴 증가하는 부분 수열.java
@@ -0,0 +1,47 @@
+import java.util.Scanner;
+ 
+public class Main {
+	
+	static int[] seq;
+	static Integer[] dp;
+	
+	public static void main(String[] args) {
+		
+		Scanner in = new Scanner(System.in);
+		
+		int N = in.nextInt();
+		
+		seq = new int[N];
+		dp = new Integer[N];
+		
+		
+		for(int i = 0; i < N; i++) {
+			seq[i] = in.nextInt();
+		}
+		
+		for(int i = 0; i < N; i++) {
+			LIS(i);
+		}
+		
+		int max = dp[0];
+		for(int i = 1; i < N; i++) {
+			max = Math.max(max, dp[i]);
+		}
+		System.out.println(max);
+	}
+	
+	
+	static int LIS(int N) {
+		
+		if(dp[N] == null) {
+			dp[N] = 1;	
+			
+			for(int i = N - 1; i >= 0; i--) {
+				if(seq[i] < seq[N]) {
+					dp[N] = Math.max(dp[N], LIS(i) + 1);
+				}
+			}
+		}
+		return dp[N];
+	}
+}
