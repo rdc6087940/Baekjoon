98a226a2327ec27319d185fb9b4c0d493fbfe38a
InCheol Kim <84650497+rdc6087940@users.noreply.github.com>
2024-03-31 22:33:54 +0900

    Create 002_광고.java

---
+++ 단계 테스트/39_문자열 알고리즘 1/002_광고.java
@@ -0,0 +1,31 @@
+import java.io.*;
+
+public class Main {
+
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int n = Integer.parseInt(br.readLine());
+		
+		String p = br.readLine();
+		
+		int[] t = makeTable(p);
+		System.out.println(n-t[n-1]);
+	}
+	
+	static int[]  makeTable(String pattern) {
+		int n = pattern.length();
+		int[] table = new int[n];
+		int idx=0;
+		
+		for(int i=1; i<n; i++) {
+			while(idx>0 && pattern.charAt(i)!=pattern.charAt(idx)) {
+				idx = table[idx-1];
+			}
+			if(pattern.charAt(i) == pattern.charAt(idx)) {
+				table[i] = ++idx;
+			}
+		}
+		return table;
+		
+	}
+}
